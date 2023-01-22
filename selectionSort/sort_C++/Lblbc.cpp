a9927cf2b3cf87143405093e8678d30155d1d174
jerry <309032663@qq.com>
2023-01-22 21:37:00 +0800

    update

---
+++ selectionSort/sort_C++/Lblbc.cpp
@@ -0,0 +1,49 @@
+﻿/// 厦门大学计算机专业 | 前华为工程师
+/// 专注《零基础学编程系列》  http://lblbc.cn/blog
+/// 包含：Java | 安卓 | 前端 | Flutter | iOS | 小程序 | 鸿蒙
+/// 公众号：蓝不蓝编程
+/// 
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void printArray(int* array, int count)
+{
+    for (int i = 0; i < count; i++)
+    {
+        cout << array[i] << " ";
+    }
+}
+
+
+void sort(int* array, int count)
+{
+    for (int i = 0; i < count - 1; i++)
+    {
+        int minIndex = i;
+        int min = array[i];
+        for (int j = i + 1; j < count; j++)
+        {
+            if (min > array[j])
+            {
+                min = array[j];
+                minIndex = j;
+            }
+        }
+        if (minIndex != i)
+        {
+            array[minIndex] = array[i];
+            array[i] = min;
+        }
+    }
+}
+
+int main()
+{
+	int array[5] = { 2, 1, 5, 4, 3 };
+	int count = sizeof(array) / sizeof(int);
+	cout << "排序前: "; printArray(array,count);
+	sort(array,count);
+	cout << "排序后: "; printArray(array,count);
+	return 0;
+}
